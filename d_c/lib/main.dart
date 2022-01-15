dc17f54689c46d314921be1cf264ffb240a60817
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-01-15 18:32:26 +0300

    merge left and right Dice. if we click the right or left it shoude be both of them change

--- d_c/lib/main.dart
+++ d_c/lib/main.dart
@@ -36,6 +36,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(() {
                   leftDiceNumber = Random().nextInt(6) + 1; // 1 + 6
+                  rightDiceNumber = Random().nextInt(6) + 1; // 1 + 6
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -45,6 +46,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1; // 1 + 6
                   rightDiceNumber = Random().nextInt(6) + 1; // 1 + 6
                 });
               },
